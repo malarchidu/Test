0b2e3e67d1d9dc7603fc49e354770b0c482f3be2
malarchidu <malar@LAPTOP-00QR25KJ>
2018-05-16 13:23:50 -0700

    First commit

---
+++ Test/src/TestPackage/string.java
@@ -0,0 +1,22 @@
+package TestPackage;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a1="tekarch";
+		String a2="tekarch";
+
+		String s1=new String("tek");
+		String s2=new String("tek");
+
+		System.out.println(a1.equals(a2));
+		System.out.println(a1==a2);
+
+		System.out.println(s1.equals(s2));
+		System.out.println(s1==s2);
+		System.out.println("hello");
+
+	}
+
+}
